c23a806ed280ea20e8519adaa911322560a9f69f
“Iris <liuirisny@gmail.com>
2025-04-09 13:37:52 -0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning> {/* mismatch between what we render between server v client render for users system preference, here's to ignore */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider> {/* wrapping into client compoenent doesn't turn the entire app into client component */}
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes' /* usethme hook from next theme package */
+
+import { Button } from '@/components/ui/button'
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+
+export default function ThemeToggle() { /* don't run on server since no access to the theme */
+  const { setTheme, resolvedTheme } = useTheme() /* function to set the theme and resolved theme what ever resolved from user system: dark or light*/
+  const [mounted, setMounted] = useState(false) /* only render toggle if this theme is mounted */
+
+  useEffect(() => {
+    setMounted(true) /* if mounted, return button */
+  }, [])
+
+  if (!mounted) { /* if unmounted, return null */
+    return null
+  }
+
+  return (
+    <Button
+      size='sm'
+      variant='ghost'
+      onClick={() => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+      }}
+    >
+      {resolvedTheme === 'dark' ? (
+        <SunIcon className='size-4 text-orange-300' />
+      ) : (
+        <MoonIcon className='size-4 text-sky-950' />
+      )}
+
+      <span className='sr-only'>Toggle theme</span>
+    </Button>
+  )
+}
