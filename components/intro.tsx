d6bb2bc2c385fbbbbd1251ee4cfcd86b17315c5d
“Iris <liuirisny@gmail.com>
2025-04-18 22:55:08 -0400

    updates profile pic

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Welcome to my playground.</h1> {/* add titleto global css */}
+        <h1 className='title no-underline'>Welcome to my lab.</h1> {/* add titleto global css */}
         <p className='mt-3 font-light text-muted-foreground'>
           I&#39;m Iris Liu, a founder and angel investor based in NYC. I enjoy creating things in my spare time, like building useful web apps and experimenting with AI developers tools.
         </p>
