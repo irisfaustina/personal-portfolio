e66bb4ee977283779461c4315a33438c8aba1a78
“Iris <liuirisny@gmail.com>
2025-05-14 20:08:55 -0400

    removes project image

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Welcome to my lab.</h1> {/* add titleto global css */}
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m Iris Liu, a founder and angel investor based in NYC. I enjoy creating things in my spare time, like building useful web apps and experimenting with AI developers tools.
+          I&#39;m Iris Liu, a tech founder and angel investor based in NYC. Lately, I've been iterating on building useful AI native productivity tools. I plan to update my progress here. Have questions or want to collab? Find me on social media or subscribe to my newsletter.
         </p>
       </div>
       <div className='relative'>
